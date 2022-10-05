5b7346617ff712fff609f5079fe837dd967d30e6
artur <artur.borodziej@gmail.com>
2022-10-05 18:40:23 +0200

    Skeleton application

---
+++ src/main/java/org/b0ro/cdqtaskapi/CdqTaskApiApplication.java
@@ -0,0 +1,13 @@
+package org.b0ro.cdqtaskapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdqTaskApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdqTaskApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/b0ro/cdqtaskapi/CdqTaskApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.b0ro.cdqtaskapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdqTaskApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
